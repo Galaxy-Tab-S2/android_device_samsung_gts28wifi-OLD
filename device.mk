22f11d1f194b0f2f27ee7541279722c2ed7a039e
CTXz <patrick_pedersen_02032001@yahoo.com>
2015-12-15 18:38:10 +0100

    sensors : Build sensor HAL from tree
    Change-Id: I2a4b2aceeeab59a532127e41a6e437ea10fba8cd

--- device.mk
+++ device.mk
@@ -14,5 +14,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     init.modem.rc
 
+###########################################################
+### Sensors
+###########################################################
+
+PRODUCT_PACKAGES += \
+    sensors.exynos5
+
 PRODUCT_NAME := full_gts28wifi
 PRODUCT_DEVICE := gts28wifi
